2e77d522563c677adb45b7a6138532765002f98b
Ash Vogl <ashvogl99@gmail.com>
2026-05-26 00:56:00 -0500

    wait attempt

--- obsidian-setup.sh
+++ obsidian-setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-ob login --email $OBSIDIAN_EMAIL --password $OBSIDIAN_PASSWORD
-sleep 15
+ob login --email $OBSIDIAN_EMAIL --password $OBSIDIAN_PASSWORD &
+wait $!
 ob sync-setup --vault Illustris --path illustris --password $VAULT_PASSWORD
 ob sync-config --path illustris --mode mirror-remote --device-name site-auto
 ob sync --path illustris
