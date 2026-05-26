8fee2a84b8516c3185cd1933d4541985ce582cd6
Ash Vogl <ashvogl99@gmail.com>
2026-05-26 00:28:42 -0500

    testing script file alternative

---
+++ obsidian-setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+ob login --email $OBSIDIAN_EMAIL --password $OBSIDIAN_PASSWORD
+ob sync-setup --vault Illustris --path illustris --password $VAULT_PASSWORD
+ob sync-config --path illustris --mode mirror-remote --device-name site-auto
+ob sync --path illustris
