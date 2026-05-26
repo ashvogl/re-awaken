036690148a6873e05eec015db9d1d2a99934a6f9
Ash Vogl <ashvogl99@gmail.com>
2026-05-26 00:32:48 -0500

    what the hell man.

--- obsidian-setup.sh
+++ obsidian-setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 ob login --email $OBSIDIAN_EMAIL --password $OBSIDIAN_PASSWORD
+ob login
 ob sync-setup --vault Illustris --path illustris --password $VAULT_PASSWORD
 ob sync-config --path illustris --mode mirror-remote --device-name site-auto
 ob sync --path illustris
